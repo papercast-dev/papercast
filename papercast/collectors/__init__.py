cc64c4e7ab3d995f96018fed6c11197a34a8fcb0
Gabriel Simmons <simmons.gabe@gmail.com>
2023-04-19 20:09:05 -0700

    remove collectors dir

--- papercast/collectors/__init__.py
+++
@@ -1,6 +0,0 @@
-# from papercast.plugin_utils import load_plugins
-
-# _installed_plugins = load_plugins("collectors")
-
-# for name, plugin in _installed_plugins.items():
-#     globals()[name] = plugin
