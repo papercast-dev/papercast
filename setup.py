0a8609dcfd78bdb1649903610eb660d0c0188164
Gabriel Simmons <simmons.gabe@gmail.com>
2023-04-19 18:27:19 -0700

    bump version 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="papercast",
-    version="0.1.0",
+    version="0.2.0",
     install_requires=[
         "fastapi",
         "uvicorn",
