18a8ca24282ddf384689d419bbc74ab86070fa81
Gabriel Simmons <simmons.gabe@gmail.com>
2023-04-19 19:13:23 -0700

    update docstrings for pipelines.py

--- papercast/pipelines.py
+++ papercast/pipelines.py
@@ -17,10 +17,29 @@ class Pipeline:
         self.executor = ThreadPoolExecutor()
 
     def _validate_name(self, name: str):
+        """
+        Checks if a processor with the given name already exists in the pipeline.
+
+        Args:
+            name (str): The name to be checked.
+
+        Raises:
+            ValueError: If a processor with the given name already exists in the pipeline.
+        """
         if name in [p.name for p in self.processors.values()]:
             raise ValueError(f"Processor with name {name} already exists")
 
     def add_processor(self, name: str, processor: BasePipelineComponent):
+        """
+        Adds a processor to the pipeline.
+
+        Args:
+            name (str): The name of the processor to be added.
+            processor (papercast.base.BasePipelineComponent): The processor to be added.
+
+        Raises:
+            ValueError: If a processor with the given name already exists in the pipeline.
+        """
         self._validate_name(name)
         setattr(processor, "name", name)
 
@@ -36,6 +55,18 @@ class Pipeline:
             self.downstream_processors[name] = processor
 
     def connect(self, a_name: str, a_output: str, b_name: str, b_input: str):
+        """
+        Connects two processors in the pipeline.
+
+        Args:
+            a_name (str): The name of the first processor.
+            a_output (str): The name of the output type of the first processor.
+            b_name (str): The name of the second processor.
+            b_input (str): The name of the input type of the second processor.
+
+        Raises:
+            TypeError: If the output type of the first processor does not match the input type of the second processor.
+        """
         a_type = self.processors[a_name].output_types[a_output]
         b_type = self.processors[b_name].input_types[b_input]
 
@@ -66,6 +97,12 @@ class Pipeline:
 
     @property
     def input_types(self) -> Dict[str, Any]:
+        """
+        Returns a dictionary of input types for all collectors in the pipeline.
+
+        Returns:
+            Dict[str, Any]: A dictionary of input types, where the keys are input names and the values are input types.
+        """
         input_types = {}
         for processor in self.collectors.values():
             input_types.update(processor.input_types)
@@ -92,8 +129,15 @@ class Pipeline:
     def get_downstream_processors(
         self, collector_subscriber_name: str
     ) -> Iterable[str]:
-        """Get all processors downstream of the collector with name `collector_name`
+        """
+        Get all processors downstream of the collector with name `collector_name`
         by recursively traversing the graph of connections.
+
+        Args:
+            collector_subscriber_name (str): The name of the collector or subscriber.
+
+        Returns:
+            Iterable[str]: A list of processor names.
         """
         downstream_processors = set()
 
@@ -124,6 +168,13 @@ class Pipeline:
             )
 
     async def _run_in_server(self):
+        """
+        Runs all subscribers in the pipeline asynchronously.
+
+        This method starts a new event loop and runs each subscriber in a separate task. The subscribers
+        are executed in parallel using the `asyncio.gather` function. This function is
+        used in :class:`papercast.server.Server` to run the pipeline in a separate thread.
+        """
         await asyncio.gather(*[self._run_subscriber(name) for name in self.subscribers])
 
     def process(
