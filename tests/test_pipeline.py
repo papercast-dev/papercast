252c3749fd842b0290f95c9ccb21c3d91f66485c
Gabriel Simmons <simmons.gabe@gmail.com>
2024-08-09 13:18:55 -0700

    add tests

---
+++ tests/test_pipeline.py
@@ -0,0 +1,20 @@
+import pytest
+from typing import Any, List, Union
+
+from papercast.pipelines import Pipeline
+
+
+class TestPipeline:
+    @pytest.mark.parametrize(
+        "a,b,result",
+        [
+            (int, int, True),
+            (int, Any, True),
+            (Any, int, False),
+            (List[int], List[int], True),
+            (List[int], List[Any], True),
+            (int, Union[int, float], True),
+        ],
+    )
+    def test_is_subtype(self, a, b, result):
+        assert Pipeline.is_subtype(a, b) == result
---
+++ tests/test_server.py
@@ -0,0 +1,6 @@
+import pytest
+from papercast.server import Server
+
+class TestServer:
+    def test_server(self):
+        assert True
