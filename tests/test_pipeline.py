9168534400207c638425fb98bbde283be3c98318
Gabriel Simmons <simmons.gabe@gmail.com>
2024-08-09 14:13:46 -0700

    pipeline tests

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,5 +1,5 @@
 import pytest
-from typing import Any, List, Union
+from typing import Any, List, Union, Dict
 
 from papercast.pipelines import Pipeline
 from papercast.base import BaseProcessor, Production
@@ -13,10 +13,17 @@ class MyInheritedClass(MyClass):
     pass
 
 
+class AnotherClass:
+    pass
+
+
 class TestPipeline:
     @pytest.mark.parametrize(
         "a,b,result",
         [
+            (List, Dict, False),
+            (Dict, List, False),
+            (int, float, False),
             (int, int, True),
             (int, Any, True),
             (Any, int, False),
@@ -26,6 +33,9 @@ class TestPipeline:
             (int, Union[float, str], False),
             (MyClass, MyInheritedClass, False),
             (MyInheritedClass, MyClass, True),
+            (MyClass, MyClass, True),
+            (MyInheritedClass, MyInheritedClass, True),
+            (MyClass, AnotherClass, False),
         ],
     )
     def test_is_subtype(self, a, b, result):
@@ -51,3 +61,37 @@ class TestPipeline:
         with pytest.raises(ValueError):
             pipeline._validate_name("test")
         pipeline._validate_name("valid_name")
+
+    def test_topological_sort(self):
+        "Test that the topological sort works correctly."
+
+        class MyProcessor(BaseProcessor):
+            input_types = {
+                "input1": int,
+            }
+            output_types = {
+                "output": int,
+            }
+
+            def process(self, input: Production) -> Production:
+                return input
+
+        processor = MyProcessor()
+
+        pipeline = Pipeline("default")
+        pipeline.add_processor("test", processor)
+
+        pipeline.add_processor("test2", processor)
+        pipeline.connect("test", "output", "test2", "input1")
+
+        pipeline.add_processor("test3", processor)
+        pipeline.connect("test2", "output", "test3", "input1")
+
+        pipeline.add_processor("test4", processor)
+        pipeline.connect("test3", "output", "test4", "input1")
+
+        sorted_processors = pipeline._topological_sort(
+            processor_names=["test", "test2", "test3", "test4"]
+        )
+
+        assert sorted_processors == ["test", "test2", "test3", "test4"]
